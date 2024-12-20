2dc74bb288d5d72686bb1cc358312f2205e176cd
KasZaim <122385481+KasZaim@users.noreply.github.com>
2024-12-20 15:47:46 +0100

    created imprint and privacy-policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { StartComponent } from './start/start.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 
 export const routes: Routes = [
-    { path: "", component: StartComponent },
+{ path: "", component: StartComponent },
+{ path: "privacy-policy", component: PrivacyPolicyComponent },
+    { path: "legal-notice", component: LegalNoticeComponent },
 ];
--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
-
+import {MatButtonModule} from '@angular/material/button';
+import { FooterComponent } from "../footer/footer.component";
 @Component({
   selector: 'app-start',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, MatButtonModule, FooterComponent],
   templateUrl: './start.component.html',
   styleUrl: './start.component.scss'
 })
