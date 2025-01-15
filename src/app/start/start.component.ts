f02c79b9222bb855c21903766cedc55e167088a6
KasZaim <122385481+KasZaim@users.noreply.github.com>
2025-01-15 14:47:18 +0100

    bugfix startpage

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -5,17 +5,27 @@ import {MatButtonModule} from '@angular/material/button';
 import { FooterComponent } from "../footer/footer.component";
 import { Router } from "@angular/router";
 import { SharedService } from '../shared.service';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-start',
   standalone: true,
-  imports: [HeaderComponent, MatButtonModule, FooterComponent,CommonModule],
+  imports: [HeaderComponent, MatButtonModule, FooterComponent,CommonModule, FormsModule],
   templateUrl: './start.component.html',
   styleUrl: './start.component.scss'
 })
 export class StartComponent {
+  email: string = '';
+  isValidEmail: boolean = false;
+
 
   constructor(public router: Router, private sharedService: SharedService) {}
 
+
+  validateEmail(email: string): void {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    this.isValidEmail = emailRegex.test(email); 
+  }
   sendValueAndNavigate(value: string) {
     this.sharedService.updateValue(value);
     this.router.navigate(['/signup']);
