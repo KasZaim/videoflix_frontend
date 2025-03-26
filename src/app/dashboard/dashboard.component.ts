972f109fae900634a52dfe49c13bce54c9ae3d7c
KasZaim <122385481+KasZaim@users.noreply.github.com>
2025-03-26 16:57:10 +0100

    added validation of sign up and log in

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { MatIconModule } from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { VideoService } from './../service/video.service';
+import { Video } from './../interfaces/video.interface';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -9,13 +14,24 @@ import {MatButtonModule} from '@angular/material/button';
   imports: [
     HeaderComponent,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    CommonModule,
+    HttpClientModule
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
 
+export class DashboardComponent implements OnInit {
+  videos: Video[] = [];
+
+  constructor(private videoService: VideoService) {}
+
+  ngOnInit(): void {
+    this.videoService.getVideos().subscribe((data) => {
+      this.videos = data;
+    });
+  }
 
   
 }
