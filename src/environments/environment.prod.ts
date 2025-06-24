de7ee61a99fa7f1552e74afc87fb6a0307c5344b
KasZaim <122385481+KasZaim@users.noreply.github.com>
2025-06-24 16:23:11 +0200

    changed name of the base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    NG_APP_API_BASE_URL: 'https://videoflixapi.kaserm.dev/'  // deine Production API URL
+    API_BASE_URL: 'https://videoflixapi.kaserm.dev/'  // deine Production API URL
   };
   
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  NG_APP_API_BASE_URL: 'http://localhost:4200/api'  // z.B. lokale API URL
+  API_BASE_URL: 'http://localhost:4200/api'  // z.B. lokale API URL
 };
